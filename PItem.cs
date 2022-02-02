6658bf0dbcf488c9380317ff1a00c5c4e2823e00
Yusril Ihsanul Alim <Yusril.edmodo25@gmail.com>
2022-02-03 05:29:59 +0700

    Add project files.

---
+++ PItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Win32HooksDemo
+{
+public class PItem
+{
+    public string ProcessName { get; set; }
+    public string Title { get; set; }
+
+    public PItem(string processname, string title)
+    {
+        this.ProcessName = processname;
+        this.Title = title;
+    }
+
+    public override string ToString()
+    {
+        if (!string.IsNullOrEmpty(Title))
+            return string.Format("{0} ({1})", this.ProcessName, this.Title);
+        else
+            return string.Format("{0}", this.ProcessName);
+    }
+}
+}
---
+++ WinPosition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Win32HooksDemo
+{
+    public class WinPosition
+    {
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public int Right { get; set; }
+        public int Bottom { get; set; }
+
+        public WinPosition()
+        {
+        }
+
+        internal WinPosition(NativeMethods.TITLEBARINFO pti)
+        {
+            this.Left = pti.rcTitleBar.left;
+            this.Top = pti.rcTitleBar.top;
+            this.Right = pti.rcTitleBar.right;
+            this.Bottom = pti.rcTitleBar.bottom;
+        }
+    }
+}
